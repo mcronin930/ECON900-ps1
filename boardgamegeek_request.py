0f293fbac9d0dd1e494fe84753154fe928240277
unknown <you@example.com>
2019-03-19 01:29:26 -0400

    selenium webdriver loop finished

--- boardgamegeek_request.py
+++ boardgamegeek_request.py
@@ -46,34 +46,3 @@ while i > 0:
     # The random number generator initiates random sleep times for the loop
     t = 10 + random.randint(-5, 5)
     time.sleep(t)
-
-
-# i = 1
-# while i > 0:
-#      print(i)
-#      file_name = "html_files/boardgame_{}.html".format(i)
-#      print(file_name)
-#      f = open(file_name, "wb")
-#      url = "https://boardgamegeek.com/browse/boardgame/page/{}".format(i)
-#      print(url)
-#      response = urllib.request.urlopen(url)
-#      html = response.read()
-#      f.write(html)
-#      f.close()
-#
-#      # The purpose of parsing the html here is to see if the table on the page is empty
-#      # if the table is empty, then all the board game data has been scraped and the program will stop requesting
-#      f = open(file_name, "r",  encoding="utf8")
-#      soup = BeautifulSoup(f.read(), 'html.parser')
-#      f.close()
-#      table = soup.find("table", {"id": "collectionitems"})
-#      empty = table.find_all("td")
-#      if empty == []:
-#          i = 0
-#      else:
-#          i = i+1
-#      print(i)
-#
-#      # The random number generator initiates random sleep times for the loop
-#      t = 10 + random.randint(-5, 5)
-#      time.sleep(t)
