8a29bf5802bae49ef17f3f0b772af6702d8cea7c
unknown <you@example.com>
2019-03-19 01:28:09 -0400

    selenium webdriver loop finished

--- boardgamegeek_request.py
+++ boardgamegeek_request.py
@@ -14,28 +14,38 @@ if not os.path.exists("html_files"):
 	os.mkdir("html_files")
 
 
-i = 1
-browser = webdriver.Firefox()
-url = "https://boardgamegeek.com/browse/boardgame/page/1059"
-browser.get(url)
-response = browser.page_source
+i = 1055
+while i > 0:
+    print(i)
+    file_name = "html_files/boardgame_{}.html".format(i)
+    print(file_name)
+    html_file = open(file_name, "w", encoding="utf8")
 
-html_file = open("selenium_test.html", "w", encoding="utf8")
-html_file.write(response)
-html_file.close
+    url = "https://boardgamegeek.com/browse/boardgame/page/{}".format(i)
+    print(url)
+    browser = webdriver.Firefox()
+    browser.get(url)
+    response = browser.page_source
 
-browser.close()
+    html_file.write(response)
+    html_file.close
 
-f = open("selenium_test.html", "r", encoding="utf8")
-soup = BeautifulSoup(f.read(), 'html.parser')
-f.close()
-table = soup.find("table", {"id": "collectionitems"})
-empty = table.find_all("td")
-if empty == []:
-     i = 0
-else:
-     i = i+1
-print(i)
+    browser.close()
+
+    f = open(file_name, "r", encoding="utf8")
+    soup = BeautifulSoup(f.read(), 'html.parser')
+    f.close()
+    table = soup.find("table", {"id": "collectionitems"})
+    empty = table.find_all("td")
+    if empty == []:
+         i = 0
+    else:
+         i = i+1
+    print(i)
+
+    # The random number generator initiates random sleep times for the loop
+    t = 10 + random.randint(-5, 5)
+    time.sleep(t)
 
 
 # i = 1
