0ae49258bee4f85166e53c472d303681bc791532
unknown <you@example.com>
2019-04-14 20:42:04 -0400

    finished loop in linear model program that will iterate through and impute all missing price types

--- 4_boardgamegeek_linearmodel.py
+++ 4_boardgamegeek_linearmodel.py
@@ -3,30 +3,35 @@ import pandas as pd
 
 dataset = pd.read_csv("training_data/boardgame_price_dataset.csv")
 
-
+d = {}
 print(dataset.head())
-Training = dataset.dropna(subset=['avg_rating', 'geek_rating', 'rank', 'votes'])
+Training = dataset.dropna(subset=['avg_rating', 'geek_rating', 'rank', 'votes', 'year'])
+Training['game_age'] = 2019 - Training['year']
+
+types = ['List', 'L_Amazon', 'N_Amazon', 'App', 'Amazon']
+
+for i in types:
+    OLS_Training = Training.loc[(Training['price_type'] == i)].dropna(subset=['price'])
+    Pred_Missing = Training.loc[(Training['price_type'] == i)][pd.isnull(Training['price'])]
+    # print(OLS_Training)
+    # print(Pred_Missing)
+
+    Data = OLS_Training.iloc[:,[1,3,7,8,-1]].values
+    Target = OLS_Training.iloc[:,13]
+    # print(Data)
+    # print(Target)
+    #
+    # ## construct a linear regression machine (as we did witht he KNN machine)
+    regression = linear_model.LinearRegression()
+
+    regression.fit(Data, Target)
+
+    OLS_predict = Pred_Missing.iloc[:,[1,3,7,8,-1]]
+    print(OLS_predict)
+
+    results = regression.predict(OLS_predict)
+    print(results)
 
 
-List_Training = Training.loc[(Training['price_type'] == 'List')].dropna(subset=['price'])
-List_Missing = Training.loc[(Training['price_type'] == 'List')][pd.isnull(Training['price'])]
-
-print(List_Training)
-print(List_Missing)
-
-List_Data = List_Training.iloc[:,[1,3,7,8]].values
-print(List_Data)
-List_Target = List_Training.iloc[:,-1]
-print(List_Target)
-#
-# ## construct a linear regression machine (as we did witht he KNN machine)
-regression = linear_model.LinearRegression()
-#
-regression.fit(List_Data, List_Target)
-
-List_predict = List_Missing.iloc[:,[1,3,7,8]]
-print(List_predict)
-
-results = regression.predict(List_predict)
-print(results)
-print(regression.score(List_Data, List_Target))
+    d["R_2_{0}".format(i)] = regression.score(Data, Target)
+    print(d["R_2_{0}".format(i)])
